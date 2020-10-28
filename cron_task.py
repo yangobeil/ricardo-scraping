a9e943a18d2345d874575b2242004f6acf905958
Yan Gobeil <yan.gobeil@decathlon.com>
2020-10-28 19:33:25 -0400

    switch to using mongoDB instead of pickle file

--- cron_task.py
+++ cron_task.py
@@ -2,20 +2,32 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import os
-import pickle
 import smtplib
 import ssl
 import time
 
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
+from pymongo import MongoClient
 import requests
 
+# load environment variables
 load_dotenv()
-port = 465  # For SSL
-password = os.getenv('SENDER_EMAIL_PASSWORD')
-sender_email = os.getenv('SENDER_EMAIL_ADDRESS')
-receiver_email = os.getenv('RECEIVER_EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.getenv('SENDER_EMAIL_PASSWORD')
+SENDER_EMAIL = os.getenv('SENDER_EMAIL_ADDRESS')
+RECEIVER_EMAIL = os.getenv('RECEIVER_EMAIL_ADDRESS')
+MONGO_USERNAME = os.getenv("MONGO_USERNAME")
+MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
+
+# connect to mongoDB
+host = f'mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@recipes-3neas.mongodb.net/Ricardo?retryWrites=true&w=majority'
+client = MongoClient(host=host)
+db = client.Ricardo
+collection = db.recipes
+print("Connected to mongoDB")
+
+# setup global variables
+PORT = 465  # For SSL
 error = None
 
 try:
@@ -25,14 +37,14 @@ try:
     main_soup = BeautifulSoup(main_page.content, features="lxml")
     num_recipes = int(main_soup.find('a', {'href': '/recherche/mot-cle//tab/recipe/page/1/facet/'}).find('span').contents[0][1:-1])
     num_pages = num_recipes // 20
+    print("Found {} pages of recipes".format(num_pages))
 
     # load old list of recipes
-    with open('recipes_list.pkl', 'rb') as file:
-        old_recipes = pickle.load(file)
+    old_recipes = collection.find()
 
     # extract list of recipes on website
     downloaded_recipes = []
-    for i in range(num_pages):
+    for i in range(4):
         search_url = "https://www.ricardocuisine.com/recherche/mot-cle//page/{}".format(str(i+1))
         page = requests.get(search_url)
         soup = BeautifulSoup(page.content, features="lxml")
@@ -46,12 +58,14 @@ try:
             print(i)
         time.sleep(5)
 
+    print(len(downloaded_recipes))
     old_recipes_titles = [item['title'] for item in old_recipes]
     new_recipes = [item for item in downloaded_recipes if item['title'] not in old_recipes_titles]
+    print(len(new_recipes))
 
     # save new list of recipes
-    with open('recipes_list.pkl', 'wb') as file:
-        pickle.dump(downloaded_recipes, file)
+    if len(new_recipes) > 0:
+        collection.insert_many(new_recipes)
 
     print("Found the list of recipes")
 
@@ -79,8 +93,8 @@ def create_email(new_recipes, error):
 
 message = MIMEMultipart("alternative")
 message["Subject"] = "Nouvelles recettes de Ricardo"
-message["From"] = sender_email
-message["To"] = receiver_email
+message["From"] = SENDER_EMAIL
+message["To"] = RECEIVER_EMAIL
 
 text, html = create_email(new_recipes, error)
 
@@ -92,9 +106,9 @@ message.attach(part2)
 # Create a secure SSL context
 try:
     context = ssl.create_default_context()
-    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message.as_string())
+    with smtplib.SMTP_SSL("smtp.gmail.com", PORT, context=context) as server:
+        server.login(SENDER_EMAIL, EMAIL_PASSWORD)
+        server.sendmail(SENDER_EMAIL, RECEIVER_EMAIL, message.as_string())
     print("Email has been sent")
 except Exception as e:
     print("There was an error when sending the email: {}".format(e))
