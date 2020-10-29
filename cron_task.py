9bf857c08f84d245d7f1288b3d1563cf156a6453
Yan Gobeil <yan.gobeil@decathlon.com>
2020-10-28 20:47:22 -0400

    increase num pages for test

--- cron_task.py
+++ cron_task.py
@@ -43,7 +43,7 @@ try:
 
     # extract list of recipes on website
     downloaded_recipes = []
-    for i in range(5):
+    for i in range(10):
         search_url = "https://www.ricardocuisine.com/recherche/mot-cle//page/{}".format(str(i+1))
         page = requests.get(search_url)
         soup = BeautifulSoup(page.content, features="lxml")
